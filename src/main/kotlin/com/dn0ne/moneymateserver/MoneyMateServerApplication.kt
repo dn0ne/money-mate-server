19122d06dfac526dd1e6846567ec568c3aea730e
dn0ne <mmmdanone@gmail.com>
2024-03-07 14:55:11 +0200

    Move controller to appropriate folder

--- src/main/kotlin/com/dn0ne/moneymateserver/MoneyMateServerApplication.kt
+++ src/main/kotlin/com/dn0ne/moneymateserver/MoneyMateServerApplication.kt
@@ -2,17 +2,9 @@ package com.dn0ne.moneymateserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
-@RestController
-class MoneyMateServerApplication {
-    @GetMapping("/hello")
-    fun hello(): String {
-        return "Hello World"
-    }
-}
+class MoneyMateServerApplication
 
 fun main(args: Array<String>) {
     runApplication<MoneyMateServerApplication>(*args)
