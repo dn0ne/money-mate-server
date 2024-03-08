45263a64cc0fe7c32acbd6973a95d41501d05037
dn0ne <mmmdanone@gmail.com>
2024-03-08 16:25:41 +0200

    Enable CORS

--- src/main/kotlin/com/dn0ne/moneymateserver/security/SecurityConfig.kt
+++ src/main/kotlin/com/dn0ne/moneymateserver/security/SecurityConfig.kt
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
@@ -29,9 +30,7 @@ class SecurityConfig(
         authenticationManager: AuthenticationManager
     ): SecurityFilterChain {
         return http
-            .cors {
-                it.disable()
-            }
+            .cors(Customizer.withDefaults())
             .csrf {
                 it.disable()
             }
